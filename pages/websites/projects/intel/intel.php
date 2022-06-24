9f357134f68cce592ed97224e90275e561351872
Andrew Loehr <andrew@kismetllc.com>
2022-06-24 15:42:14 -0400

    add intel to websites

---
+++ pages/websites/projects/intel/intel.php
@@ -0,0 +1,46 @@
+<div class="website row" aria-label="intel">
+	<div class="website-content">
+		<h3>Intel Design-in Tools</h3>
+		<p>Bango Bowls is an açaí and smoothie restaurant based out of the Northeast. They have multiple locations and needed a website to showcase them all as well as their full menu for each location with an integrated&nbsp;online ordering tool.</p>
+		<p><strong>Powered by:</strong> Magento / ScandiPWA</p>
+		<p><strong>Tools used:</strong> React, PHP, SCSS, Composer, NPM, Yarn, Docker</p>
+	</div>
+	<div class="website-screenshots loading">
+		<div class="website-logo">
+			<picture>
+				<!--[if IE 9]><video style='display: none;'><![endif]--><!--[if IE 9]></video><![endif]-->
+				<img src="/pages/websites/projects/bango-bowls/images/bango-bowls-logo.png" alt="" class="img-responsive">
+			</picture>
+		</div>
+		<div class="website-image website-image-a">
+			<picture>
+				<!--[if IE 9]><video style='display: none;'><![endif]--><!--[if IE 9]></video><![endif]-->
+				<img src="/pages/websites/projects/bango-bowls/images/bango-bowls-homepage.jpg" alt="" class="img-responsive">
+			</picture>
+		</div>
+		<div class="website-image website-image-b">
+			<picture>
+				<!--[if IE 9]><video style='display: none;'><![endif]--><!--[if IE 9]></video><![endif]-->
+				<img src="/pages/websites/projects/bango-bowls/images/bango-bowls-our-story.jpg" alt="" class="img-responsive">
+			</picture>
+		</div>
+		<div class="website-image website-image-c">
+			<picture>
+				<!--[if IE 9]><video style='display: none;'><![endif]--><!--[if IE 9]></video><![endif]-->
+				<img src="/pages/websites/projects/bango-bowls/images/bango-bowls-contact-us.jpg" alt="" class="img-responsive">
+			</picture>
+		</div>
+		<div class="website-image website-image-d">
+			<picture>
+				<!--[if IE 9]><video style='display: none;'><![endif]--><!--[if IE 9]></video><![endif]-->
+				<img src="/pages/websites/projects/bango-bowls/images/bango-bowls-locations.jpg" alt="" class="img-responsive">
+			</picture>
+		</div>
+		<div class="website-image website-image-e">
+			<picture>
+				<!--[if IE 9]><video style='display: none;'><![endif]--><!--[if IE 9]></video><![endif]-->
+				<img src="/pages/websites/projects/bango-bowls/images/bango-bowls-massapequapark.jpg" alt="" class="img-responsive">
+			</picture>
+		</div>
+	</div>
+</div>
